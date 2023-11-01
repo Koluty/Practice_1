7c80c0adeb6017b8ef35d62c43f1c5b18eed0e1f
Максим Котов <kotovmaks374@gmail.com>
2023-11-01 18:03:15 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Створення об'єкту класу табулювання
+            Tabul tabul = new Tabul();
+
+            double xn, xk, h, a;
+            //Отримання даних
+            xn = Convert.ToDouble(textBox1.Text);
+            xk = Convert.ToDouble(textBox2.Text);
+            h = Convert.ToDouble(textBox3.Text);
+            a = Convert.ToDouble(textBox4.Text);
+
+            //Очищення грід та графіку
+            dataGridView1.Rows.Clear();
+            chart1.Series[0].Points.Clear();
+            tabul.tab(xn, xk, h, a);
+
+            for (int i = 0; i < tabul.n; i++)
+            {
+                dataGridView1.Rows.Add(Math.Round(tabul.xy[i,0],2).ToString(),
+                    Math.Round(tabul.xy[i, 1], 3).ToString());
+                chart1.Series[0].Points.AddXY(tabul.xy[i, 0], tabul.xy[i, 1]);
+            }
+        }
+    }
+}
